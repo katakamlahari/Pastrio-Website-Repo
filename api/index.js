b8b9c83cb516d5796e9a9214bc5d5ba6e5d5d378
lahari <katakamlahari977@gmail.com>
2025-12-29 22:11:58 +0530

    Use direct Express handler in Vercel function wrapper (avoid serverless-http)

--- api/index.js
+++ api/index.js
@@ -1,17 +1,13 @@
-const serverless = require('serverless-http');
 const { app, connectDB } = require('../server');
-
-const handler = serverless(app);
 
 module.exports = async (req, res) => {
   try {
     await connectDB();
   } catch (err) {
     console.error('DB connection error in serverless handler:', err);
-    res.statusCode = 500;
-    res.end('Database connection error');
+    res.status(500).send('Database connection error');
     return;
   }
 
-  return handler(req, res);
+  return app(req, res);
 };
