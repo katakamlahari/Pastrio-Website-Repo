a8f2e31a1e5e79a48cbe6f408392a19b8c2df219
lahari <katakamlahari977@gmail.com>
2025-12-30 08:54:55 +0530

    Add Vercel serverless entry: api/index.js (wrap Express app with serverless-http)

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
+const serverless = require('serverless-http');
+const path = require('path');
+
+// Import the Express app exported by server.js
+const { app, connectDB } = require(path.join(__dirname, '..', 'server'));
+
+// Ensure DB connection is established (returns a promise)
+connectDB().catch((err) => {
+  console.error('Error connecting to DB in serverless wrapper:', err && err.message ? err.message : err);
+});
+
+module.exports = serverless(app);
 const { app, connectDB } = require('../server');
 
 module.exports = async (req, res) => {
